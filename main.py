8cacf0da0d241a0dbd3967c1bb89bb5f36e09b11
Tunmise <tunmistom@gmail.com>
2026-05-20 08:16:13 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-from fastapi import FastAPI, HTMLResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import auth
+from routes import auth, docs
 from core.config import settings
 from database.db_config import Base, engine
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    docs_url=None,
-    redoc_url=None,
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # to be changed in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(auth.router)
+# app.include_router(docs.router)
 
 Base.metadata.create_all(bind=engine)
 
--- routes/docs.py
+++ routes/docs.py
@@ -1,9 +1,9 @@
-from fastapi import HTMLResponse
-from fastapi.routing import APIRouter
+from fastapi.responses import HTMLResponse
+from fastapi import APIRouter
 from services.custom_docs_service import CustomDocsService
 
 router = APIRouter()
-@router.get("/docs", response_model=HTMLResponse, tags=["Documentation"])
+@router.get("/docs", response_class=HTMLResponse, tags=["Documentation"])
 def custom_docs():
     html_content = CustomDocsService.read_html_custom_docs("utils\\documentation_frontend\\docs.html")
     return HTMLResponse(content=html_content)
